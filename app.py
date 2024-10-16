592aa7210a3f571e0463060ed765c47717ca7b20
lenn0n13 <lennonbenedictjansuy@gmail.com>
2024-10-16 11:05:44 +0800

    Init files

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, Response
+from flask_cors import CORS
+
+import pandas as pd
+import replicate
+
+app = Flask(__name__)
+cors = CORS(app)
+
+@app.route('/')
+def home():
+  return "Python Flask API for Story Generation Page and Main Time in PH"
+
+# Libs: PANDAS, CSV
+@app.route('/api/v1/retrieve-time', methods=['POST', 'OPTIONS'])
+def retrieve_time():
+  data = request.json
+  area = data.get('area')
+
+  df = pd.read_csv('data.csv')
+  x = []
+  y = []
+
+  for rt in df['RUN_TIME']:
+    x.insert(len(x), rt)
+
+  for ar in df[area]:
+    y.insert(len(y), ar)
+  
+  return { "data": { "x": x, "y": y} }
+
+# Libs: REPLICATE
+@app.route('/api/v1/generate-story', methods=['POST', 'OPTIONS'])
+def generate_story():
+  data = request.json
+  query = data.get('query')
+
+  input_data = {
+    'prompt': query,
+    'max_tokens': 514,
+    'min_tokens': 10,
+    'prompt_template': "{prompt}"
+  }
+
+  def stream_response():
+    for event in replicate.stream(
+      "meta/meta-llama-3-8b",
+      input=input_data,
+    ):
+      yield str(event)
+
+  return Response(stream_response(), content_type='text/plain')
+
+if __name__ == '__main__':
+  app.run(debug=False)
